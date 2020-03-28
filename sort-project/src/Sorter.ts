462ace6832e488727e27cd0f312437ccfc03febf
Kim Skovhus Andersen <kimskovhusandersen@gmail.com>
2020-03-28 12:27:14 +0100

    Minor refactor

--- sort-project/src/Sorter.ts
+++ sort-project/src/Sorter.ts
@@ -1,23 +1,21 @@
 interface Sortable {
-  length: number;
-  compare(leftIndex: number, rightIndex: number): boolean;
-  swap(leftIndex: number, rightIndex: number): void;
+    length: number;
+    compare(leftIndex: number, rightIndex: number): boolean;
+    swap(leftIndex: number, rightIndex: number): void;
 }
 
 export class Sorter {
-  // collection: number[];
-  constructor(public collection: Sortable) {
-    // this.collection = collection
-  }
-  sort(): void {
-    const { length } = this.collection;
-
-    for (let i = 0; i < length; i++) {
-      for (let j = 0; j < length - i - 1; j++) {
-        if (this.collection.compare(j, j + 1)) {
-          this.collection.swap(j, j + 1);
-        }
-      }
+    constructor(public collection: Sortable) {
     }
-  }
+    sort(): void {
+        const { length, compare, swap } = this.collection;
+
+        for (let i = 0; i < length; i++) {
+            for (let j = 0; j < length - i - 1; j++) {
+                if (compare(j, j + 1)) {
+                    swap(j, j + 1);
+                }
+            }
+        }
+    }
 }
